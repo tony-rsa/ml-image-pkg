e931945c8caea97583f30cba0c3a137ff7ab6513
Thonifho Muhali <tmuhali@student.wethinkcode.co.za>
2022-03-27 01:49:28 +0000

    image visualize inline, plot ad window

---
+++ image/visualize/window/windows.py
@@ -0,0 +1,14 @@
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+Copy
+When you want to have plot popout, just do this:
+
+%matplotlib qt
+Copy
+You can switch back to inline mode again by doing:
+
+%matplotlib inline
